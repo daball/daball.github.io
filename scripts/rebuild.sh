41dc7167b850e4a77d9fc2c44ba072da81569a45
David Ball <david@daball.me>
2020-03-10 20:17:37 -0400

    Updated build script

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/bin/zsh
 # cd ..
+echo "Removing ./resume/dist"
 rm -rf resume/dist
+echo "Making directory ./resume/dist"
 mkdir resume/dist
+echo "Copying .git repo to ./resume/dist"
 cp -R .git resume/dist
+echo "Entering ./resume/dist"
 cd resume/dist
+echo "Checking out master branch"
 git checkout master
+echo "Removing current files/folders"
 rm -rf 2010
 rm -rf 2011
 rm -rf 2012
@@ -22,16 +28,22 @@ rm -rf svg
 rm -rf vendor
 rm ads.txt
 rm index.html
+echo "Entering ./resume"
 cd ..
 # cd resume
+echo "Building JavaScript site"
 gulp
+echo "Entering ./hugo"
 cd ..
 cd hugo
+echo "Building Go site"
 hugo --minify
+echo "Entering ./"
 cd ..
+echo "Merginng Go site artifacts to JavaScript site"
 mv hugo/public/index.html resume/dist/
 mv hugo/public/plugins resume/dist/
 mv hugo/public/images resume/dist/
 mv hugo/public/css/* resume/dist/css/
 mv hugo/public/js resume/dist/
-mv hugo/public/assets resume/dist
+# mv hugo/public/assets resume/dist
